97af9a51ae3a7caada301c491e8d3da4d8ea1d95
deepesh-sr <220714100078@cutm.ac.in>
2025-08-14 00:07:53 +0530

    cleaning the trustplay version 1

--- Trustplay_Capstone_Project/programs/joinroom/src/constants.rs
+++
@@ -1,5 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[constant]
-pub const SEED: &str = "anchor";
-pub const MAX_PLAYER : u8 = 10;
--- Trustplay_Capstone_Project/programs/joinroom/src/state/participant.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-#[derive(InitSpace)]
-pub struct Participant{
-    pub participant_player : Pubkey,
-    pub room : Pubkey,
-}
